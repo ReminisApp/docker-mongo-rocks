448041a7a378caf8f6675ef6120638087a69f290
Andrew Chen <yongjhih@gmail.com>
2016-10-08 06:29:17 +0800

    Support random password

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,10 +9,14 @@ set -e
 if [ "$MONGO_ROOT_PASSWORD" ]; then
 	mongo admin --eval "db.createUser({user: 'root', pwd: '$MONGO_ROOT_PASSWORD', roles:[{role:'root',db:'admin'}]});"
 fi
+
+MONGO_USER=${MONGO_PASSWORD:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)}
+MONGO_PASSWORD=${MONGO_PASSWORD:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)}
 if [ "$MONGO_PASSWORD" ]; then
 	mongo admin --eval "db.createUser({user: '$MONGO_USER', pwd: '$MONGO_PASSWORD', roles:[{role:'root',db:'$MONGO_DATABASE'}]});"
 fi
 touch /data/db/.mongodb_password_set
+echo mongodb://${MONGO_USER}:${MONGO_PASSWORD}@$(hostname):27017/${MONGO_DATABASE}
 
 if [ "${1:0:1}" = '-' ]; then
 	set -- mongod "$@" --storageEngine rocks
